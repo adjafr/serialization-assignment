94a80cc27be87340b3424f668b95764377bcf055
Adam Fraser <AJFraser10@GMail.com>
2016-08-10 20:02:18 -0500

    completed serialization-assignment - fixed student wrap error

--- src/main/java/com/cooksys/serialization/assignment/model/Session.java
+++ src/main/java/com/cooksys/serialization/assignment/model/Session.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
@@ -16,6 +18,8 @@ public class Session {
 	@XmlAttribute(name = "start-date")
     private String startDate;
     private Instructor instructor;
+    @XmlElementWrapper
+    @XmlElement(name = "student")
     private List<Student> students;
 
     public String getLocation() {
